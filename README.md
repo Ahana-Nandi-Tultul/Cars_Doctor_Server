ae172673f0382f8ce5c040d7901ae12ca346914d
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-22 15:45:02 +0600

    Vercel Deploy and README file add

---
+++ README.md
@@ -0,0 +1,8 @@
+# Car Doctor Server: 
+This is the server side of car doctor. Car doctor is a MERN Project
+
+## Live-server: https://car-doctor-server-five-sigma.vercel.app/
+
+## Packeages: I have used:
+* JWT for secure tokenized connection
+* MonogoDB as database
--- index.js
+++ index.js
@@ -21,6 +21,24 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  // console.log("From verify jwt");
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: "unauthorized access"});
+  }
+
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if(error){
+      return res.status(401).send({error: true, message: "unauthorized access"});
+    }
+
+    req.decoded = decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -51,8 +69,13 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/bookings', async(req, res) => {
+    app.get('/bookings', verifyJWT, async(req, res) => {
         // console.log(req.query.email);
+        const decoded = req.decoded;
+        console.log(decoded);
+        if(decoded.email !== req.query.email){
+          return res.status(403).send({error: true, message: "forbiden accesss"})
+        }
         let query = {};
         if(req.query?.email){
             query = {email: req.query.email}
